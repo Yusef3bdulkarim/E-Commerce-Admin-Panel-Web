f1b6683672d3db7c33eb9919c55b56f14d8798fe
Yusef3bdulkarim <hamadayosseef@gmail.com>
2026-04-15 03:40:41 +0200

    complate Banner Screen UI

--- lib/feature/banner_screen/UI/screen/edit_banner/responsive/desktop_edit_banner.dart
+++ lib/feature/banner_screen/UI/screen/edit_banner/responsive/desktop_edit_banner.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../../../../../../core/common/widgets/breadcrumb/breadrumb_with_heading.dart';
+import '../../../../../../core/routes/routing_helper.dart';
+import '../../../../../../core/utils/constants/sizes.dart';
+import '../widget/edit_banner_form.dart';
 
 class DesktopEditBannerScreen extends StatelessWidget {
   const DesktopEditBannerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TBreadcrumbWithHeading(
+              heading: 'Update Banner',
+              breadcrumbItems: [TRoutingHelper.bannerScreen, "Update Brand"],
+              returnToPreviousScreen: true,
+            ),
+            Gap(TSizes.spaceBtwSections),
+            EditBannerForm(),
+          ],
+        ),
+      ),
+    ));
   }
 }
---
+++ lib/feature/banner_screen/UI/screen/edit_banner/widget/edit_banner_form.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../../../../../../core/common/widgets/containers/rounded_container.dart';
+import '../../../../../../core/common/widgets/images/t_rounded_image.dart';
+import '../../../../../../core/utils/constants/colors.dart';
+import '../../../../../../core/utils/constants/enums.dart';
+import '../../../../../../core/utils/constants/image_strings.dart';
+import '../../../../../../core/utils/constants/sizes.dart';
+
+class EditBannerForm extends StatelessWidget {
+  const EditBannerForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TRoundedContainer(
+      width: 500,
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gap(TSizes.sm),
+            Text(
+              'Update Banner',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Gap(TSizes.spaceBtwInputFields * 2),
+            Column(
+              children: [
+                TRoundedImage(
+                  imageType: ImageType.asset,
+                  width: 400,
+                  height: 200,
+                  backgroundColor: TColors.primaryBackground,
+                  image: TImages.defaultImage,
+                ),
+                Gap(TSizes.spaceBtwItems),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Select Image'),
+                ),
+              ],
+            ),
+            Gap(TSizes.spaceBtwInputFields),
+            Text(
+              'Make your Banner Active or InActive',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Gap(TSizes.spaceBtwInputFields),
+            CheckboxMenuButton(
+              value: true,
+              onChanged: (_) {},
+              child: const Text('Active'),
+            ),
+            Gap(TSizes.spaceBtwInputFields),
+            DropdownButton<String>(
+              items: const [
+                DropdownMenuItem<String>(
+                  value: 'home',
+                  child: Text('Home'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'search',
+                  child: Text('search'),
+                ),
+              ],
+              onChanged: (String? newValue) {},
+              value: 'search',
+            ),
+            Gap(TSizes.spaceBtwInputFields * 2),
+            SizedBox(
+              width: double.infinity,
+              child:
+                  ElevatedButton(onPressed: () {}, child: const Text('Create')),
+            ),
+            Gap(TSizes.spaceBtwInputFields * 2),
+          ],
+        ),
+      ),
+    );
+  }
+}
